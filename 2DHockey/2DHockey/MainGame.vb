6e2a82822194320eaa2c0f7009da949da384f27d
edabonacci <edabonacci@gmail.com>
2015-06-16 17:57:01 -0400

    Fixed conflict
    Fixed conflicts caused by previous merge.

--- 2DHockey/2DHockey/MainGame.vb
+++ 2DHockey/2DHockey/MainGame.vb
@@ -235,11 +235,7 @@
     Sub checkForGoal()
         If objectCollisionDetect(puck, compNet) And puck.Location.X > userNet.Location.X + userNet.Width - 10 Then 'checks if puck is touching net and is past net
             goalScored("user")
-<<<<<<< HEAD
-        ElseIf objectCollisionDetect(puck, compNet) And puck.Location.X + puck.Width < compNet.Location.X + 10 Then
-=======
         ElseIf objectCollisionDetect(puck, userNet) And puck.Location.X + puck.Width < compNet.Location.X + 10 Then
->>>>>>> origin/master
             goalScored("comp")
         End If
     End Sub
